67a4fba04f2b9096df065ccee172ec3aed494d26
Mieszko Wrzeszczyński <miewrz@st.amu.edu.pl>
2018-12-02 16:26:27 +0100

    Fix sorting

--- train_chopin_midi/midiworld/mazurkas/concatenate_all_csv.sh
+++ train_chopin_midi/midiworld/mazurkas/concatenate_all_csv.sh
@@ -4,9 +4,30 @@ set -eu
 set -o pipefail
 
 output_file_name=$1
+touch "$output_file_name"
+temp_file=$(mktemp  -t tmp.XXXXXXXXXX)
+function finish {
+    rm -rf "$temp_file"
+}
+trap finish EXIT
+
 
 last_time_value=0
-for file in *.csv; do
-    awk   -F',' 'BEGIN { OFS = FS } { $2 = $2 +  "'"$last_time_value"'"; print }' "$file" >> "$output_file_name"
-    last_time_value=$(tail -n 1 "$file"  |  cut -d ',' -f2)
+for file in mazrka*.csv; do
+
+    before=$(tail -n 1 "$file"  |  cut -d ',' -f2)
+    awk   -F',' 'BEGIN { OFS = FS } { $2 = $2 +  "'"$last_time_value"'"; print }' "$file" > "$temp_file"
+    after=$(tail -n 1 "$temp_file"  |  cut -d ',' -f2)
+    last_time_value=$(tail -n 1 "$temp_file"  |  cut -d ',' -f2)
+    echo -r "$file\t$last_time_value\t$before\t$after"
+    cat "$temp_file" >> "$output_file_name"
 done
+
+
+if $(cut -d "," -f2 "$output_file_name" | sort -cn); then
+    echo "Sorted properly"
+    cut -d ',' -f2,3,5,6 "$output_file_name" | sponge  "$output_file_name"
+else
+    echo "Sorted improperly"
+    exit 1
+fi
